205af2135b7db157c3cb3d70094f3aebd3d0298a
SlashedAsteroid <freefall1103@hotmail.co.uk>
2015-06-25 17:53:02 +0100

    Unit Creation Modifiers
    Buildings now use stackable modifiers to indicate unit creation, upon
    selecting to create a multiple unit the duration count is set to the
    remaining time on its first cycle.
    
    These modifiers seem to reset the building model, so to counter this on
    BattleBeyond_updateModifier( event ) SetModel() is called to prevent the
    building "disappearing".

--- scripts/vscripts/buildings/init.lua
+++ scripts/vscripts/buildings/init.lua
@@ -4,7 +4,7 @@ BattleBeyond_unitTypes['building_village'] = {}
 BattleBeyond_unitTypes['building_village']['model'] = "models/props_structures/good_ancient001.vmdl"
 BattleBeyond_unitTypes['building_village']['model_scale'] = 0.6
 BattleBeyond_unitTypes['building_village']['collision_hull'] = 160
-BattleBeyond_unitTypes['building_village']['abilities'] = { "battlebeyond_ability_unit_create_settler" }
+BattleBeyond_unitTypes['building_village']['abilities'] = { "battlebeyond_ability_unit_create_settler", "battlebeyond_ability_unit_create_worker"  }
 
 BattleBeyond_unitTypes['building_barracks'] = {}
 BattleBeyond_unitTypes['building_barracks']['model'] = "models/props_structures/good_barracks_melee001.vmdl"
@@ -23,13 +23,7 @@ function BattleBeyond_createBuilding( event )
     local owner = caster:GetOwner()
     local player = caster:GetPlayerOwner()
     local unitType = event.unitType
-    local origin = Vector( 0, 0, 0 )
-    
-    if ( unitType == 'building_village' ) then
-        origin = caster:GetOrigin()
-    else
-        origin = event.target_points[1]
-    end
+    local origin = ( unitType == 'building_village' ) and caster:GetOrigin() or event.target_points[1]
     
     local building = CreateUnitByName( "battlebeyond_building_template", origin, true, owner, owner, caster:GetTeamNumber() )
     building:RemoveModifierByName( "modifier_invulnerable" )
@@ -37,6 +31,7 @@ function BattleBeyond_createBuilding( event )
     building:SetControllableByPlayer( player:GetPlayerID(), true )
     building:SetAbsOrigin( origin )
     building:SetHullRadius( BattleBeyond_unitTypes[unitType]['collision_hull'] )
+    building.Type = unitType
     Timers:CreateTimer(0.01, function()
             building:SetModel( BattleBeyond_unitTypes[unitType]['model'] )
             building:SetModelScale( BattleBeyond_unitTypes[unitType]['model_scale'] )
@@ -81,4 +76,30 @@ function BattleBeyond_createUnit( event )
                 unit:MoveToPosition( caster.rally_point:GetAbsOrigin() )
         end )
     end
+end
+
+function BattleBeyond_updateModifier( event )
+    local caster = event.caster
+    local modifier_name = event.Modifier
+    local event_type = event.Type
+    local modifier = caster:FindModifierByName( modifier_name )
+    local count = caster:GetModifierStackCount( modifier_name, caster )
+    local increase = ( event_type == "battlebeyond_event_increase_stack" ) and true or false
+    
+    if ( modifier:GetDuration() == -1 ) then
+        modifier:SetDuration( 5, true )
+    end
+    
+    if ( increase ) then
+        modifier:SetDuration( modifier:GetRemainingTime(), true )
+        caster:SetModifierStackCount( modifier_name, caster, count + 1 )
+    else
+        caster:SetModifierStackCount( modifier_name, caster, count - 1 )
+        BattleBeyond_createUnit( event )
+        modifier:SetDuration( 5, true )
+        if ( count == 1 ) then
+            caster:RemoveModifierByName( modifier_name )
+        end
+    end
+    caster:SetModel( BattleBeyond_unitTypes[caster.Type]['model'] )
 end
